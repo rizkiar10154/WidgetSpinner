0991cf28a8bae0fc2b6de7502f19052e6264a5ff
rizkiar10154 <rizkyarif196@gmail.com>
2019-10-07 23:08:59 +0700

    WidgetSpinner

---
+++ app/src/main/java/com/example/widgetspinner/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.widgetspinner
+
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val makanan = arrayOf("Nasi Goreng", "Nasi Ayam", "Nasi Pecel", "Sego Saduk")
+        val Spinner= findViewById<Spinner>(R.id.spinner)
+        if (Spinner != null) {
+            val arrayAdapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, makanan)
+            spinner.adapter = arrayAdapter
+
+            spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                    Toast.makeText(this@MainActivity, getString(R.string.selected_item) + " " + makanan[position], Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // Code to perform some action when nothing is selected
+                }
+            }
+        }
+    }
+}
+
